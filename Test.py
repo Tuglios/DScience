98bf7eb72fdbc774ee03d60eb7870d62f0ea9113
Tuglios <gerthhh@gmail.com>
2025-02-02 13:48:09 +0200

    Random2

--- Test.py
+++ Test.py
@@ -1,5 +1,59 @@
-print("Test Message")
-print(-111)
-print(123.4)
+from operator import truediv
 
-print(None)
+print("Test Message") #String
+print(-111)
+print(123.4) #Float
+
+print(None) #Null or None
+print(False) #Boolean
+
+print(type("Test Message"))
+print(type(123.5))
+print(type(False))
+print(['Inimene1','Inimene2','Inimene 3'])
+print([{
+    'name': 'Inimene1',
+    'age': 28,
+    'address': 'This street, That City'
+}])
+#Excercise1
+
+print("This is a standard text sequence.") #string
+print(-4) #integer?
+print(0) #integer?
+print(13.676) #Float
+print(True) #Boolean
+print(None) #Null
+print(b'This is a byte text sequence.') #? String Sequence?
+print([1,2,3,4,5,6,7,8]) #list
+print([{'name' : 'xyz'}]) #string
+
+
+#Excercise2
+"""
+score = 80
+
+if score >= 90:
+    result = 'A'
+elif score >= 80:
+    result = 'B'
+elif score >= 70:
+    result = 'C'
+else:
+    result = 'F'
+
+print(result)
+"""
+age = 17
+has_license = 0
+
+if age >= 18 and has_license >= 1:
+    result = 'Adult with license'
+elif age >= 18 and has_license <= 0:
+    result = 'Adult without license'
+elif age < 18:
+    result = 'Minor'
+
+print(result)
+
+#End
--- Test2.py
+++ Test2.py
@@ -1,4 +1,22 @@
-print("Welcome")
-
 print(None)
+#Data
+age = 90
+name = 'Hesus'
+address = 'See, on, aadress'
+gender = 'Male'
+isHuman = 'True'
+isMarried = 'None'
+phoneNumber = [112233,4445523,44422545]
+thingsinbag = [{
+    'money': 100,
+    'food':['chocolate','nuts','cheese','bread']
+}]
+#Show
 
+print(age,name)
+print(gender,
+      isHuman)
+print(isMarried)
+print(phoneNumber)
+print(thingsinbag)
+#End
